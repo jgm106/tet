55697e900e479a8fb9905d2c65d8727db2f7190f
Jeff Minelli <jgm106@psu.edu>
2014-01-10 14:39:31 -0500

    sdf

--- grab.js
+++ grab.js
@@ -3,17 +3,18 @@ function callback(data) {
   if(data.message.indexOf(',') === 0) {
     var msg = data.message;
     var fromID = data.fromID;
+    var myID = API.getUser().fromID;
     var command = msg.substring(1).split(' ');
     if(typeof command[2] != "undefined"){
       for(var i = 2; i<command.length; i++){
         command[1] = command[1] + ' ' + command[i];
       }
     }
-    if(API.getUser(fromID) == API.getUser()) {
+    if(fromID == myID) {
       switch(command[0].toLowerCase()){
         case "grab":
           $(".icon-curate").click();
-          $($(".curate").children(".menu").children().children()[command[1]-1]).mousedown();
+          $($(".curate").children(".menu").children().children()[command[1]]).mousedown();
           break;
       }
     }
