21f3a1060a08fc3d30fa88328eab4d2206c6a060
Jeff Minelli <jgm106@psu.edu>
2014-01-10 11:42:24 -0500

    sdf

--- grab.js
+++ grab.js
@@ -1,35 +1,19 @@
 API.on(API.CHAT, callback);
 function callback(data) {
-  API.sendChat('bAAnjoBOT Grabber');
-}
-
-/*
-API.on(API.CHAT, function(data){
-  if(data.message.indexOf(',') === 0){
-    var msg = data.message, from = data.from, fromID = data.fromID;
+  if(data.message.indexOf(',') === 0) {
+    var msg = data.message;
     var command = msg.substring(1).split(' ');
     if(typeof command[2] != "undefined"){
       for(var i = 2; i<command.length; i++){
         command[1] = command[1] + ' ' + command[i];
       }
     }
-      switch(command[0].toLowerCase()){
-        case "add":
-            if(API.getUser(fromID).permission > 1 || Countrybot.admins.indexOf(fromID) > -1){
-                $(".icon-curate").click();
-                $($(".curate").children(".menu").children().children()[0]).mousedown();
-              }
-              break;
-
-        case "test":
-          if(Countrybot.admins.indexOf(fromID) > -1){
-            API.sendChat("@"+ data.from +" Test Successful");
-          }else{
-            API.sendChat("This command requires bAAnjo only!");
-          }
-          break;
-      }
+    switch(command[0].toLowerCase()){
+      case "grab":
+        $(".icon-curate").click();
+        $($(".curate").children(".menu").children().children()[0]).mousedown();
+        break;
+    }
   }
-});
+}
 
-*/
